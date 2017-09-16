eb2c0aed3033360d41a605718296da2ad70abba3
Gleb <parakhonskiyg@gmail.com>
2017-09-16 21:25:04 +0200

    Fixed regression algorithm

--- src/main/scala/hackerrank/PolyRegressionProblem.scala
+++ src/main/scala/hackerrank/PolyRegressionProblem.scala
@@ -8,7 +8,8 @@ import fs2._
 import scala.annotation.tailrec
 
 object PolyRegressionProblem extends App {
-  val learningRate = 1d
+  val learningRate = 0.1d
+  val err = 0.000001d
 
   val data: Stream[IO, (Vector[Double], Double)] = io.file
     .readAll[IO](Paths.get("src/main/resources/testdata.txt"), 4096)
@@ -21,12 +22,6 @@ object PolyRegressionProblem extends App {
   // B = [b0, b1, b2 .. bn]
   // h = BT * X
   def train(data: Stream[IO, (Vector[Double], Double)]): Vector[Double] ⇒ Double = {
-    /*
-    * Calculate the hypothesis h = X * theta
-    * Calculate the loss = h - y and maybe the squared cost (loss^2)/2m
-    * Calculate the gradient = X' * loss / m
-    * Update the parameters theta = theta - alpha * gradient
-    * */
     val all: Vector[(Vector[Double], Double)] = data.runLog.unsafeRunSync()
     val (xs, ys): (Vector[Vector[Double]], Vector[Double]) = all.unzip
     val thetas = Vector.fill(xs.head.size + 1)(0d)
@@ -35,39 +30,43 @@ object PolyRegressionProblem extends App {
     hypo(readyThetas)
   }
 
-
   // it is our matrix. xs is [x1, x2] for 2 features
   def hypo(thetas: Vector[Double]): Vector[Double] ⇒ Double = xs ⇒ {
     (thetas zip xs map (d ⇒ d._1 * d._2)).sum
   }
 
+  // X.size = 100; x.head.size = 3
   @tailrec
   def go(thetas: Vector[Double], X: Vector[Vector[Double]], Y: Vector[Double]): Vector[Double] = {
-    val x0 = Vector.fill(X.head.size)(1d)
-    val xs = x0 +: X
-    val err = 0.000001d
-
+    val coef = learningRate / Y.size
     val updThetas = for {
-      (θ, xjs) ← thetas zip xs
-      coef = learningRate / X.size
-      summ = for {
-        (xi, yi) ← X zip Y
-        xj ← xjs
-        diff = hypo(thetas)(xi) - yi
-      } yield diff * xj
-    } yield θ - summ.sum * coef
-    if ((thetas zip updThetas) forall { case (o, n) ⇒ Math.abs(o - n) < err }) updThetas
-    else {
-//      println(printlns"refined model: $updThetas")
-      go(updThetas, X, Y)
+      j ← thetas.indices
+      θj = thetas(j)  // X - hundred elems, here it should be one  (XJS -> vector of 2 elems)
+      l = loss(j, thetas, X, Y)
+    } yield {
+      θj - l * coef
     }
+
+    if ((thetas zip updThetas) forall { case (o, n) ⇒ Math.abs(o - n) < err }) updThetas.toVector
+    else go(updThetas.toVector, X, Y)
   }
 
+  private def loss(j: Int, thetas: Vector[Double], X: Vector[Vector[Double]], Y: Vector[Double]): Double = (for {
+    i ← Y.indices
+    xi = 1d +: X(i)
+    l = hypo(thetas)(xi) - Y(i)
+  } yield l * xi(j)).sum
 
-  println(train(data)(Vector(0.05, 0.54)))
-  println(train(data)(Vector(0.91, 0.91)))
-  println(train(data)(Vector(0.31, 0.76)))
-  println(train(data)(Vector(0.51, 0.31)))
+  val predict = train(data)
+
+
+  println(predict(Vector(0.44, 0.68))) // should be 511.14
+  println(predict(Vector(1.0, 0.23)))  // should be 726.69
+
+  println(predict(Vector(0.05, 0.54))) // should be 180.38
+  println(predict(Vector(0.91, 0.91))) // should be 1312.07
+  println(predict(Vector(0.31, 0.76))) // should be 440.13
+  println(predict(Vector(0.51, 0.31))) // should be 343.72
 }
 
 object Data {
